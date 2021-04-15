49789fc3bf0c4a3d54e24f3e6d1a9f6ef9f90ee4
TheDobler <46963445+TheDobler@users.noreply.github.com>
2021-04-15 20:38:00 +0200

    Update PlaySoundWhenPressAKey.py
    Add some comments.

--- PlaySoundWhenPressAKey.py
+++ PlaySoundWhenPressAKey.py
@@ -3,14 +3,14 @@ import keyboard
 
 
 def sound():
-    playsound('Napalm_Death-You_Suffer.mp3')
+    playsound('') #add a mp3 file in here to play when you press the buton.
 
 
 exit = True
 
 while (exit):
 
-    if(keyboard.is_pressed('space')):
+    if(keyboard.is_pressed('space')): #Change here for which button you want to play the mp3 file.
         sound()
 
     exit = not (keyboard.is_pressed('esc'))
